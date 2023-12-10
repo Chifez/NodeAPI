a6042b7bcb502dc79930c0f46b41b53e3392119d
emmanuel <chifez1@gmail.com>
2023-12-10 21:37:31 +0000

    Created a basic CRUD Api with Nodejs,express and MongoDB

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/productsModel');
+const app = express();
+
+app.use(express.json());
+mongoose
+  .connect(
+    'mongodb+srv://chifez1:chifez10@nodeapi.pvpxpgc.mongodb.net/node-api?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('connected to mongodb');
+    app.listen(3000, () => {
+      console.log('app is running');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.post('/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      res.status(404).json('cannot find product');
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      res.status(404).json('product does not exist in this database');
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
