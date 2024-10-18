424714e74b089854f25186db267e0e880ed0e24b
emmanuel <chifez1@gmail.com>
2024-10-18 21:31:21 +0100

    Product api done

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const Product = require('../models/product');
-const { VerifyTokenAndAdmin } = require('./verifyToken');
+const {
+  VerifyTokenAndAdmin,
+  VerifyTokenAndAuthorization,
+} = require('./verifyToken');
 
-//   Create
-
+// create a product
 router.post('/', VerifyTokenAndAdmin, async (req, res) => {
   const newProduct = new Product(req.body);
 
@@ -15,7 +17,7 @@ router.post('/', VerifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// update
+// update a product
 router.post('/:id', VerifyTokenAndAdmin, async (req, res) => {
   try {
     const updatedProduct = await Product.findByIdAndUpdate(
@@ -31,12 +33,45 @@ router.post('/:id', VerifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// delete
-
+// delete a product
 router.delete('/:id', VerifyTokenAndAdmin, async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
     res.status(200).json('The product has been deleted');
+  } catch (err) {
+    res.status(500).json('An error occurred');
+  }
+});
+
+// get all product
+router.get('/', async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json('An error occured');
+  }
+});
+
+// get a product
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json('An error occurred');
   }
